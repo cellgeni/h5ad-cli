bf672893e903b899ce4c0301a11e92d511d67457
Aljes <aleksandr.binkevich@gmail.com>
2026-01-23 19:24:08 +0000

    Update import statements in __init__.py to include additional export functions

--- src/h5ad/commands/__init__.py
+++ src/h5ad/commands/__init__.py
@@ -1,4 +1,4 @@
 from h5ad.commands.info import show_info
 from h5ad.commands.subset import subset_h5ad
-from h5ad.commands.export import export_object, export_table
+from h5ad.commands.export import export_table, export_image, export_json, export_mtx, export_npy
 from h5ad.commands.import_data import import_object
